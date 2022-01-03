c6dcf1198da755bb20039ed983e885b2a637764d
Mustafizur <mustafizur.prodhan@gmail.com>
2022-01-02 20:41:45 -0500

    setup the coding exercise to begin at another time.

---
+++ sample_list.py
@@ -0,0 +1,26 @@
+''' 
+Practicing code by using print(), range(), 
+and other functions. The goal is to recall usage of method
+or loops to get the solutions. Below is a sample list that will
+be used for this exercise.
+'''
+
+sample_list = [
+    469, 755, 244, 245, 758, 450, 
+    302, 20, 712, 71, 456, 21, 398, 
+    339, 882, 848, 179, 535, 940, 472
+]
+
+# 1. print("Elements and count of sample_list:")
+
+# 2. print("Even elements and their count:")
+
+# 3. print("Odd elements and their count:")
+
+# 4. print("Squares of elements of sample_list:")
+
+# 5. print("Sum of elements of sample_list:")
+
+# 6. print("Smallest (min) element of sample_list:")
+
+# 7. print("Largest (max) element of sample_list:")
