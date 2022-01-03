ddd9f31da61a2a6b7c1235295294c00c75258d6b
Mustafizur <mustafizur.prodhan@gmail.com>
2022-01-03 17:37:06 -0500

    Practicing code.

--- sample_list.py
+++ sample_list.py
@@ -12,15 +12,44 @@ sample_list = [
 ]
 
 # 1. print("Elements and count of sample_list:")
+sample_count = 0
+for sample in sample_list:
+    counter = sample_list.count(sample)
+    sample_count += counter
+    print(sample)
+print(f" There are {sample_count} numbers.")
 
 # 2. print("Even elements and their count:")
+even_count = 0
+for sample in sample_list:
+    if sample%2 == 0:
+        counter = sample_list.count(sample)
+        even_count += counter
+        print(sample)
+print(f"There are {even_count} even numbers.")
 
 # 3. print("Odd elements and their count:")
+odd_count = 0
+for sample in sample_list:
+    if sample%2 == 1:
+        counter = sample_list.count(sample)
+        odd_count += counter
+        print(sample)
+print(f"There are {odd_count} odd numbers.")
 
 # 4. print("Squares of elements of sample_list:")
+for sample in sample_list:
+    square_sample = pow(sample, 2)
+    print(square_sample)
 
 # 5. print("Sum of elements of sample_list:")
+total = sum(sample_list)
+print(f"The total of the sample list is {total}")
 
 # 6. print("Smallest (min) element of sample_list:")
+min_sample = min(sample_list)
+print(f"The minimum sample is {min_sample}.")
 
 # 7. print("Largest (max) element of sample_list:")
+max_sample = max(sample_list)
+print(f"The maximum sample number is {max_sample}.")
